4fbc6a48f31193a0e2f29a9d66257325f1d7dc12
shadeesha <hshadeesha@gmail.com>
2025-07-11 11:37:26 +0100

    init commit

---
+++ src/main/java/com/example/dota2/Dota2MatchInsightsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dota2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dota2MatchInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dota2MatchInsightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dota2/Dota2MatchInsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dota2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dota2MatchInsightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
